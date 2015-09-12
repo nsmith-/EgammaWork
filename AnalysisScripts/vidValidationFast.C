eaebdd1fdeac01645b487715c4d90ac3ab72b1f8
Ilya Kravchenko <ikrav@unl.edu>
2015-09-12 10:59:04 -0500

    A simple script for fast VID validation

---
+++ AnalysisScripts/vidValidationFast.C
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "TFile.h"
+#include "TTree.h"
+
+using namespace std;
+
+void vidValidationFast(TString fileName, TString treeName, TString idName){
+
+  TFile *fin = new TFile(fileName);
+  if( !fin ){
+    cout << "Failed to open file " << fileName.Data() << endl;
+    return;
+  }
+  TString fullTreeName = TString::Format("ntupler/%s",treeName.Data());
+  TTree *tree = (TTree*)fin->Get(fullTreeName);
+  if( !tree ){
+    cout << "Failed to find TTree " << fullTreeName.Data() << endl;
+    return;
+  }
+
+  cout << "\nFor reference, here is the content of the tree:" << endl;
+  tree->Show(0);
+
+  TString idCut = TString::Format("%s==1",idName.Data());
+
+  int nEleTot = tree->Draw("pt","","goff");
+  int nElePass = tree->Draw("pt",idCut,"goff");
+  
+  cout << "\nQuick check for the ID " << idName.Data() << ":" << endl; 
+  cout << " total electrons : " << nEleTot << endl;
+  cout << " passed electrons: " << nElePass << endl;
+
+  return;
+}
