fe7ef51e2087e646e3aa0e0bd3e32fa13c5c58c4
Ilya Kravchenko <ikrav@unl.edu>
2015-12-22 00:23:55 +0100

    Changed the name of the Geometry include for 76X compatibility

--- ElectronNtupler/test/runElectrons_VID_CutBased_Spring15_50ns_demo.py
+++ ElectronNtupler/test/runElectrons_VID_CutBased_Spring15_50ns_demo.py
@@ -4,7 +4,7 @@ process = cms.Process("TestElectrons")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 # NOTE: the pick the right global tag!
