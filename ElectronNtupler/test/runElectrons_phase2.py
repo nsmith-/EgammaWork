f23c4e5662f6b5248a0369ef73a48bd8e0979c95
Nick Smith <nick.smith@cern.ch>
2017-04-05 18:51:10 +0200

    Phase 2 python cfg

---
+++ ElectronNtupler/test/runElectrons_phase2.py
@@ -0,0 +1,58 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+process = cms.Process("Ntupler")
+options = VarParsing('analysis')
+options.parseArguments()
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+process.load("RecoParticleFlow.PFClusterProducer.particleFlowRecHitHGC_cff")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+process.source = cms.Source ("PoolSource", fileNames = cms.untracked.vstring(options.inputFiles) )
+#
+# Configure the ntupler module
+#
+
+process.ntupler = cms.EDAnalyzer('SimpleElectronNtupler',
+                                 # The module automatically detects AOD vs miniAOD, so we configure both
+                                 #
+                                 # Common to all formats objects
+                                 #
+                                 pileup   = cms.InputTag("addPileupInfo"),
+                                 rho      = cms.InputTag("fixedGridRhoFastjetAll"),
+                                 beamSpot = cms.InputTag('offlineBeamSpot'),
+                                 genEventInfoProduct = cms.InputTag('generator'),
+                                 #
+                                 # Objects specific to AOD format
+                                 #
+                                 electrons    = cms.InputTag("ecalDrivenGsfElectrons"),
+                                 #electrons    = cms.InputTag("gedGsfElectrons"),
+                                 genParticles = cms.InputTag("genParticles"),
+                                 vertices     = cms.InputTag("offlinePrimaryVertices"),
+                                 conversions  = cms.InputTag('allConversions'),
+                                 #
+                                 # Objects specific to MiniAOD format
+                                 #
+                                 electronsMiniAOD    = cms.InputTag("slimmedElectrons"),
+                                 genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
+                                 verticesMiniAOD     = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                 conversionsMiniAOD  = cms.InputTag('reducedEgamma:reducedConversions'),
+                                 # Effective areas for computing PU correction for isolations
+                                 effAreasConfigFile = cms.FileInPath("RecoEgamma/ElectronIdentification/data/Spring15/effAreaElectrons_cone03_pfNeuHadronsAndPhotons_25ns.txt"),
+        HGCalIDToolConfig = cms.PSet(
+            HGCBHInput = cms.InputTag("HGCalRecHit","HGCHEBRecHits"),
+            HGCEEInput = cms.InputTag("HGCalRecHit","HGCEERecHits"),
+            HGCFHInput = cms.InputTag("HGCalRecHit","HGCHEFRecHits"),
+            HGCPFRecHits = cms.InputTag("particleFlowRecHitHGC::Ntupler"),
+            withPileup = cms.bool(True),
+        ),
+                                 )
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string(options.outputFile)
+                                   )
+
+
+process.p = cms.Path(process.particleFlowRecHitHGCSeq+process.ntupler)
