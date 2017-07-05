5e65b3bb9f08193e20f26cb84d1c3cc4b3e38836
Nick Smith <nick.smith@cern.ch>
2017-07-05 17:13:31 +0200

    Phase 2 photons

---
+++ PhotonNtupler/test/runPhotons_phase2.py
@@ -0,0 +1,61 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+process = cms.Process("Ntupler")
+options = VarParsing('analysis')
+options.parseArguments()
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '91X_upgrade2023_realistic_v3', '')
+#process.load("RecoParticleFlow.PFClusterProducer.particleFlowRecHitHGC_cff")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+process.source = cms.Source ("PoolSource", fileNames = cms.untracked.vstring(options.inputFiles) )
+#
+# Configure the ntupler module
+#
+process.load("RecoEgamma/PhotonIdentification/PhotonIDValueMapProducer_cfi")
+process.photonIDValueMapProducer.esReducedRecHitCollection = cms.InputTag("")
+process.photonIDValueMapProducer.esReducedRecHitCollectionMiniAOD = cms.InputTag("")
+
+process.ntupler = cms.EDAnalyzer('SimplePhotonNtupler',
+    # The module automatically detects AOD vs miniAOD, so we configure both
+    #
+    # Common to all formats objects
+    #                                    
+    rho = cms.InputTag("fixedGridRhoFastjetAll"),
+    #
+    # Objects specific to AOD format
+    #
+    photons = cms.InputTag("gedPhotons"),
+    genParticles = cms.InputTag("genParticles"),
+    #
+    # Objects specific to MiniAOD format
+    #
+    photonsMiniAOD = cms.InputTag("slimmedPhotons"),
+    genParticlesMiniAOD = cms.InputTag("prunedGenParticles"),
+    #
+    # ValueMap names from the producer upstream
+    #
+    full5x5SigmaIEtaIEtaMap   = cms.InputTag("photonIDValueMapProducer:phoFull5x5SigmaIEtaIEta"),
+    phoChargedIsolation = cms.InputTag("photonIDValueMapProducer:phoChargedIsolation"),
+    phoNeutralHadronIsolation = cms.InputTag("photonIDValueMapProducer:phoNeutralHadronIsolation"),
+    phoPhotonIsolation = cms.InputTag("photonIDValueMapProducer:phoPhotonIsolation"),
+    # 
+    # Locations of files with the effective area constants.
+    # The constants in these files below are derived for PHYS14 MC.
+    #
+    effAreaChHadFile = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring15/effAreaPhotons_cone03_pfChargedHadrons_25ns_NULLcorrection.txt"),
+    effAreaNeuHadFile= cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring15/effAreaPhotons_cone03_pfNeutralHadrons_25ns_90percentBased.txt"),
+    effAreaPhoFile   = cms.FileInPath("RecoEgamma/PhotonIdentification/data/Spring15/effAreaPhotons_cone03_pfPhotons_25ns_90percentBased.txt")
+)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string(options.outputFile)
+                                   )
+
+
+process.p = cms.Path(process.photonIDValueMapProducer * process.ntupler)
